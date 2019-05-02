a2ea55573950400e3d08beac5279a7b22185ea2a
tskotnicki <37276798+tskotnicki@users.noreply.github.com>
2019-05-02 12:34:20 +0200

    Initial commit

---
+++ Odcinek/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Odcinek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pbox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(395, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wybierz metodę:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(152, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Podaj punkt końcowy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Podaj punkt początkowy";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Metoda przyrostowa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(448, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Metoda Bresenhama";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(99, 80);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Zatwierdź dane";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(41, 51);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(48, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(180, 51);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(48, 20);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "x1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "y1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(156, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "x2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(156, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "y2";
+            // 
+            // pbox
+            // 
+            this.pbox.Location = new System.Drawing.Point(20, 121);
+            this.pbox.Name = "pbox";
+            this.pbox.Size = new System.Drawing.Size(600, 600);
+            this.pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbox.TabIndex = 14;
+            this.pbox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 744);
+            this.Controls.Add(this.pbox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rysowanie odcinka";
+            ((System.ComponentModel.ISupportInitialize)(this.pbox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pbox;
+    }
+}
+
---
+++ Odcinek/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odcinek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int x1 = 0;
+        public int y1 = 0;
+        public int x2 = 0;
+        public int y2 = 0;
+        public Bitmap tlo;
+        //zabezpieczenie wejścia
+        private void textBox1_TextChanged(object sender, EventArgs e){}
+        private void textBox2_TextChanged(object sender, EventArgs e){}
+        private void textBox3_TextChanged(object sender, EventArgs e) { }
+        private void textBox4_TextChanged(object sender, EventArgs e) { }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int wynik) == true && int.TryParse(textBox2.Text, out int wynik2) == true && int.TryParse(textBox3.Text, out int wynik3) == true && int.TryParse(textBox4.Text, out int wynik4) == true)
+            {     
+                x1 = int.Parse(textBox1.Text);
+                y1 = int.Parse(textBox3.Text);
+                x2 = int.Parse(textBox2.Text);
+                y2 = int.Parse(textBox4.Text);
+                if (x1<0)
+                {
+                    x2 += 2*Math.Abs(x1);
+                    x1 = Math.Abs(x1);
+                }
+                if (x2 < 0)
+                {
+                    x1 += 2 * Math.Abs(x2);
+                    x2 = Math.Abs(x2);
+                }
+                if (y1 < 0)
+                {
+                    y2 += 2 * Math.Abs(y1);
+                    y1 = Math.Abs(y1);
+                }
+                if (y2 < 0)
+                {
+                    y1 += 2 * Math.Abs(y2);
+                    y2 = Math.Abs(y2);
+                }
+                {
+                    MessageBox.Show("Dane zostały przyjęte.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Podno błędne dane! Podaj liczby typu int!");
+            }
+        }
+
+        //przyrostowy
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x,y,dx,dy,m;
+            int temp, xwie, ywie;
+            xwie = x2;
+            ywie = y2;
+            if (x1 > x2)
+            {
+                xwie = x1;
+                temp = x1;
+                x1 = x2;
+                x2 = temp;
+                temp = y1;
+                y1 = y2;
+                y2 = temp;
+            }
+            if (y1 > y2)
+            {
+                ywie = y1;
+            }
+            else { ywie = y2; }
+
+            dx = x2 - x1;
+            dy = y2 - y1;
+            m = dy / dx;
+            tlo = new Bitmap(xwie + 1, ywie + 1);
+
+            //osie
+            for (int i = 0; i < xwie; i++)
+            {
+                tlo.SetPixel(i, 0, Color.Black);
+            }
+            for (int j = 0; j < ywie; j++)
+            {
+                tlo.SetPixel(0, j, Color.Black);
+            }
+
+            //linia
+            if (Math.Abs(m) <= 1)
+            {
+                y = y1;
+                for (x = x1; x < x2; x++)
+                {
+                    tlo.SetPixel((int)Math.Round(x,1), (int)Math.Round(y,1), Color.Blue);
+                    y +=m;
+                }
+            }
+            else
+            {
+                x = x1;
+                for (y = y1; y < y2; y++)
+                {         
+                    tlo.SetPixel((int)Math.Round(x,1),(int)Math.Round(y,1), Color.Blue);
+                    x += (1 / m);
+                }
+            }
+            pbox.Image = tlo;
+        }
+
+        //bresenham
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int d, dx, dy, ai, bi, xi, yi, xwie, ywie;
+            int x = x1, y = y1;
+            if (x1 < x2)
+            {
+                xwie = x2;
+                xi = 1;
+                dx = x2 - x1;
+            }
+            else
+            {
+                xwie = x1;
+                xi = -1;
+                dx = x1 - x2;
+            }
+            if (y1 < y2)
+            {
+                ywie = y2;
+                yi = 1;
+                dy = y2 - y1;
+            }
+            else
+            {
+                ywie = y1;
+                yi = -1;
+                dy = y1 - y2;
+            }
+            tlo = new Bitmap(xwie+1, ywie+1);
+            //osie
+            for (int i = 0; i < xwie; i++)
+            {
+                tlo.SetPixel(i, 0, Color.Black);
+            }
+            for (int j = 0; j < ywie; j++)
+            {
+                tlo.SetPixel(0, j, Color.Black);
+            }
+            tlo.SetPixel(x, y, Color.Red);
+            //OX szybkie
+            if (dx > dy)
+            {
+                ai = (dy - dx) * 2;
+                bi = dy * 2;
+                d = bi - dx;
+                while (x != x2)
+                {
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                    }
+                    else
+                    {
+                        d += bi;
+                        x += xi;
+                    }
+                    tlo.SetPixel(x, y, Color.Red);
+                }
+            }
+            //OY szybkie
+            else
+            {
+                ai = (dx - dy) * 2;
+                bi = dx * 2;
+                d = bi - dy;
+                while (y != y2)
+                {
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                    }
+                    else
+                    {
+                        d += bi;
+                        y += yi;
+                    }
+                    tlo.SetPixel(x, y, Color.Red);
+                }
+            }
+            pbox.Image = tlo;
+        }
+    }
+}
